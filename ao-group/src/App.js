cc6b1dea16a8277552f84a5ad38280854cadf27a
Eben Burger <eben@arlowconsulting.co.za>
2021-10-08 00:03:17 +0200

    footer added and tweaked app.js

--- ao-group/src/App.js
+++ ao-group/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Nav from './components/nav'
+import Nav from './components/nav';
+import Footer from './components/footer'
 
 function App() {
   return (
 
-    <Nav />
-    
+    <>
+      <Nav />
+
+      <Footer />
+    </>
+
   );
 }
 
