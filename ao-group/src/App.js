125330a3a0d553bc035a43a9c73573b97f8aea96
Eben Burger <eben@arlowconsulting.co.za>
2021-10-08 22:54:37 +0200

    added header component

--- ao-group/src/App.js
+++ ao-group/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/nav';
+import Header from './components/header'
 import Footer from './components/footer'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
     <>
       <Nav />
-
+      <Header />
       <Footer />
     </>
 
