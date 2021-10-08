fa01fe9acd8e470e213be74c361d60b0779d65e8
Eben Burger <eben@arlowconsulting.co.za>
2021-10-09 00:47:38 +0200

    initial layout done for work and contact

--- ao-group/src/pages/contact.js
+++ ao-group/src/pages/contact.js
@@ -3,7 +3,10 @@ import React from "react";
 const Contact = () => {
     return (
         <>
-            <h1>Contact</h1>
+            <div className="w3-container" style={{padding:"128px 16px"}} id="contact">
+                <h3 className="w3-center">Contact</h3>
+                <p className="w3-center w3-large">Want to talk?</p>
+            </div>
         </>
     )
 }
--- ao-group/src/pages/work.js
+++ ao-group/src/pages/work.js
@@ -3,7 +3,10 @@ import React from "react";
 const Work = () => {
     return (
         <>
-            <h1>Work</h1>
+            <div className="w3-container" style={{padding:"128px 16px"}} id="work">
+                <h3 className="w3-center">OUR WORK</h3>
+                <p className="w3-center w3-large">What we've done for people</p>
+            </div>
         </>
     )
 }
