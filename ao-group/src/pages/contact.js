44d96582ebabddc695cc161b9a8863f3cdcaafe0
Eben Burger <eben@arlowconsulting.co.za>
2021-10-10 12:10:50 +0200

    updated toggle modal state

--- ao-group/src/pages/contact.js
+++ ao-group/src/pages/contact.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import ContactForm from "../components/contactForm";
 import ContactModal from "../components/contactModal";
 
-const Contact = () => {
+
+const Contact = ({heading, content}) => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const toggleModal = () => setIsModalOpen(!isModalOpen)
+
     return (
         <>
             <div className="w3-container" style={{padding:"128px 16px"}} id="contact">
                 <h3 className="w3-center">Contact</h3>
                 <p className="w3-center w3-large">Want to talk?</p>
             </div>
-            <ContactForm /> 
-            <ContactModal />      
+            <ContactForm
+                toggleModal={toggleModal}
+                isModalOpen={isModalOpen}
+            /> 
+            <ContactModal 
+                toggleModal={toggleModal}
+                isModalOpen={isModalOpen}
+                modalHeading={heading}
+                modalContent={content}
+                isModalOpen={isModalOpen}
+            />  
+               
         </>
     )
 }
